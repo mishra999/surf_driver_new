fafa6fe441e30f8efd97018f22926d3e9bebdf64
barawn <allison.122@osu.edu>
2018-08-29 11:12:40 -0400

    add benchmarking script

---
+++ benchmark_surf.py
@@ -0,0 +1,41 @@
+import time
+
+class benchmarker:
+    def __init__(self, dev):
+        self.dev = dev
+
+    def time_dma_only(self, nloops):
+        total_loops = nloops
+        # constant time setup
+        self.dev.write(0x40000, 0x20000)
+        self.dev.write(0x40004, 0)
+        self.dev.write(0x40008, 511)
+        start_time = time.time()
+        while nloops:
+            nloops = nloops-1
+            self.dev.labc.force_trigger()
+            self.dev.write(0x4000C, 1)
+            val = self.dev.read(0x4000C)
+            if not (val & 0x4):
+                val = self.dev.read(0x4000C)
+        end_time = time.time()
+        total_bytes = 2048*total_loops
+        total_time = end_time - start_time
+        throughput = (total_bytes/total_time)
+        print "total time: ", total_time, " throughput: ", throughput, " bytes/sec (", throughput/(1024.*1024.), " MB/s)"
+
+    def time_event(self, nloops):
+        total_loops = nloops
+        start_time = time.time()
+        while nloops:
+            nloops = nloops-1
+            self.dev.labc.force_trigger()
+            dat = self.dev.dma_lab(0)
+        end_time = time.time()
+        total_bytes = 2048*total_loops
+        total_time = end_time - start_time
+        throughput = (total_bytes/total_time)
+        print "total time: ", total_time, " throughput: ", throughput, " bytes/sec (", throughput/(1024.*1024.), " MB/s)"
+
+
+        
